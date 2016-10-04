ec639be50cac08323babbef793cb1a6889671da6
Tertiary Infotech <angch@tertiaryinfotech.com>
2016-10-04 23:46:03 +0800

    init commit

---
+++ exercises/a0.py
@@ -0,0 +1,6 @@
+country ="France"
+capital ="Paris"
+print('The capital of {} is {}'.format(country,capital))
+# if (a<b):
+# 	print('a is smaller than b')
+#print(a+b)
---
+++ exercises/script2.js
@@ -0,0 +1,3 @@
+alert("Today is a Javascript Course");
+//console.log("Today is Javascript course");
+//document.write("<h1>Javascript Essential Training</h1>");
